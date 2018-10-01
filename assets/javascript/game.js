c25880fc44959e55328c26f9c8ea00c26b62dfe2
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-09-30 19:51:12 -0600

    begin pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//create array of words for computer to choose from
+//set win counter to zero 
+//set guess counter to number
+//listen for any key press to start
+//computer randomly chooses word
+//display dashes representing word chosen
+//listen for key press to guess letter
+//if letter in word, change corresponding dash to letter else subtract from letter guess and add to array of letters guessed
