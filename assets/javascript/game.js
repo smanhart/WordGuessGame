78274767284ff1e7efc78cea4a2a8aa1427b0187
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-02 21:06:30 -0600

    started code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,29 @@
-//create array of words for computer to choose from
-//set win counter to zero 
-//set guess counter to number
-//listen for any key press to start
-//computer randomly chooses word
-//display dashes representing word chosen
-//listen for key press to guess letter
-//if letter in word, change corresponding dash to letter else subtract from letter guess and add to array of letters guessed
+//Variables
+var wordList = [
+    sunflower,
+    elderberry,
+    snapdragon,
+    lavender,
+    honeysuckle,
+    bluebells,
+    columbine,
+    orchid,
+    violet,
+    bromeliad
+];
+
+var winCount = 0;
+var guessCount = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+//Functions
---
+++ assets/javascript/outline.js
@@ -0,0 +1,21 @@
+//create array of words for computer to choose from *
+    //pick a theme *
+
+//set win counter to zero *
+//set guess counter to number *
+//listen for any key press to start (sets off function to choose word)
+//computer randomly chooses word
+    //does that word become an array of letters?
+//display dashes representing letters in word chosen
+    //print dashes equal to word.length -1?
+
+//listen for key up to guess letter (figure out how to exclude #, etc)
+    //if letter in word, change corresponding dash to letter 
+        //else if letter in letters guessed array do nothing
+        //else subtract from guess counter and push to array of letters guessed
+        //print letters guessed array to screen
+    
+//check to see if all the correct letters have been guessed
+    //if no but guess counter > 0 go back through loop
+    //if no but guess counter === 0 then you lose
+    //if yes then add to win counter
