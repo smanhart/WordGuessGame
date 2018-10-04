030f2e2be85fc73911c8f39dbd93b920b33c0839
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-04 09:56:52 -0600

    tinker with js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,7 +23,19 @@ var guessCount = 0;
 
 
 
-
-
-
 //Functions
+
+document.onkeyup = function() {
+    var activeWord = wordList[Math.floor(Math.random()*wordList.length)];
+    
+    console.log(activeWord);
+
+
+    // var activeWord = wordList[Math.floor(Math.random()*wordList.length)];
+//  if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+//      alert("you've made a valid guess, you guessed " + userGuess);
+//  } else {
+//      alert("BAD GUESS, you guessed " + userGuess);
+ }
+
+} 
--- assets/javascript/outline.js
+++ assets/javascript/outline.js
@@ -9,7 +9,7 @@
 //display dashes representing letters in word chosen
     //print dashes equal to word.length -1?
 
-//listen for key up to guess letter (figure out how to exclude #, etc)
+//listen for key up to guess letter (figure out how to exclude #, etc ==> look into event.which
     //if letter in word, change corresponding dash to letter 
         //else if letter in letters guessed array do nothing
         //else subtract from guess counter and push to array of letters guessed
